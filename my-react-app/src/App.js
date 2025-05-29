17a808fa72ad341032adb757c97253872e47f64a
smmigrino <smmigrino@student.apc.edu.ph>
2025-05-29 11:05:59 +0800

    test change

--- my-react-app/src/App.js
+++ my-react-app/src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello, we're in Vercel!
+          Testing this!
         </p>
         <a
           className="App-link"
